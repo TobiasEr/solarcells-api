742ad84dcd311dfc42dc970ca4d7b52f58fc9b1a
Tobias Eriksson <tobias.eriksson@ha.ax>
2023-12-04 12:07:51 +0200

    Implemented lifespan instead of on_event

--- solarcells-api/main.py
+++ solarcells-api/main.py
@@ -1,19 +1,17 @@
 from fastapi import FastAPI
 from routers import router as panels_router
+from contextlib import asynccontextmanager
 from database import connect_to_db, close_db_connection
 
 
-app = FastAPI()
-
-
-@app.on_event("startup")
-async def startup_db_client():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     await connect_to_db()
-
-
-@app.on_event("shutdown")
-async def shutdown_db_client():
+    yield
     await close_db_connection()
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 app.include_router(panels_router, tags=["panels"], prefix="/solar-panels")
