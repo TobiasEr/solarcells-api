3361d5fb60e814b502ad0f7d794bfb63bdf299a1
Tobias Eriksson <tobias.eriksson@ha.ax>
2023-12-04 12:08:37 +0200

    Query parameters added

--- solarcells-api/routers.py
+++ solarcells-api/routers.py
@@ -5,6 +5,7 @@ from models import PanelLog, Panel, CreateLogModel, Metadata
 from database import get_logs_collection, get_panels_collection
 from http import HTTPStatus
 from typing import List
+import datetime as dt
 
 router = APIRouter()
 
@@ -33,22 +34,49 @@ async def create_panel_log(
             panel=panel
         )
     )
-    await logs_collection.insert_one(panel_log.model_dump())
-    return panel_log
+    new_log = await logs_collection.insert_one(panel_log.model_dump())
+    created_log = await logs_collection.find_one({"_id": new_log.inserted_id})
+    return created_log
 
 
 @router.get("/{panel_id}/logs", response_model=List[PanelLog], status_code=HTTPStatus.OK)
 async def get_panel_logs(
         panel_id: int,
+        start: str | None = None,
+        end: str | None = None,
         logs_collection=Depends(get_logs_collection),
         panels_collection=Depends(get_panels_collection)
 ):
     panel = await panels_collection.find_one({"panelID": panel_id})
-
     if not panel:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=f"Panel with ID {panel_id} not found")
 
-    logs = await logs_collection.find({"metadata.panel.panelID": panel_id}).to_list(1000)
+    query = {"metadata.panel.panelID": panel_id}
+
+    dateformat = "%d-%m-%Y"
+
+    if start:
+        try:
+            start_date = datetime.strptime(start, dateformat)
+            query["timestamp"] = {"$gte": start_date}
+        except ValueError:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail=f"Start date in wrong format or invalid date. Should be dd-mm-yyyy.")
+
+    if end:
+        try:
+            end_date = datetime.strptime(end, dateformat) + dt.timedelta(minutes=1439)
+            if "timestamp" in query:
+                query["timestamp"].update({"$lte": end_date})
+            else:
+                query["timestamp"] = {"$lte": end_date}
+        except ValueError:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail=f"End date in wrong format or invalid date. Should be dd-mm-yyyy.")
+
+    logs = await logs_collection.find(query).to_list(1000)
     return logs
 
 
@@ -62,8 +90,13 @@ async def get_panel(panel_id: int, panels_collection=Depends(get_panels_collecti
 
 @router.post("", response_model=Panel, status_code=HTTPStatus.CREATED)
 async def create_panel(panel: Panel, panels_collection=Depends(get_panels_collection)):
-    await panels_collection.insert_one(panel.model_dump())
-    return panel
+    if await panels_collection.count_documents({"panelID": panel.panelID}) > 0:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail=f"Panel with ID {panel.panelID} already exists.")
+    new_panel = await panels_collection.insert_one(panel.model_dump())
+    created_panel = await panels_collection.find_one({"_id": new_panel.inserted_id})
+    return created_panel
 
 
 @router.get("", response_model=List[Panel], status_code=HTTPStatus.OK)
