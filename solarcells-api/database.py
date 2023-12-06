5b5baa5844a25f7d35846767abcdd0bfdeb16239
Tobias Eriksson <tobias.eriksson@ha.ax>
2023-12-06 22:22:28 +0200

    Added functions for saving logs to database

--- solarcells-api/database.py
+++ solarcells-api/database.py
@@ -1,5 +1,9 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from datetime import datetime
+
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 from config.config import MONGO_DB, MONGO_URL
+from fmiweather import get_latest_weather
+from models import CreateLogModel, PanelLog, Metadata, TempSensorLog
 
 
 class MongoDB:
@@ -28,6 +32,10 @@ async def get_logs_collection():
     return await mongodb.get_collection("test-collection")
 
 
+async def get_tempsensor_collection():
+    return await mongodb.get_collection("temp-sensor-data")
+
+
 async def get_panels_collection():
     return await mongodb.get_collection("panels")
 
@@ -38,3 +46,49 @@ async def connect_to_db():
 
 async def close_db_connection():
     await mongodb.close()
+
+
+async def save_log_to_database(
+        log: CreateLogModel,
+        panel_id: int,
+        logs_collection: AsyncIOMotorCollection,
+        panel_collection: AsyncIOMotorCollection
+):
+    panel = await panel_collection.find_one({"panelID": panel_id})
+    if not panel:
+        return None
+    panel_log = PanelLog(
+        timestamp=datetime.now().replace(microsecond=0),
+        metadata=Metadata(
+            effect=log.effect,
+            weather=get_latest_weather(),
+            panel=panel
+        )
+    )
+    new_log = await logs_collection.insert_one(panel_log.model_dump())
+    created_log = await logs_collection.find_one({"_id": new_log.inserted_id})
+    return created_log
+
+
+async def save_temp_log_to_database(
+        vrm_sensor_data,
+        tempsensor_collection: AsyncIOMotorCollection
+):
+    sensor_logs = await vrm_sensor_data()
+    time_now = datetime.now().replace(microsecond=0)
+    weather = get_latest_weather()
+    sensor_logs = [
+        TempSensorLog(
+            timestamp=time_now,
+            sensordata=sensor_logs[0],
+            weather=weather
+        ),
+        TempSensorLog(
+            timestamp=time_now,
+            sensordata=sensor_logs[1],
+            weather=weather
+        )
+    ]
+    inserted_logs = await tempsensor_collection.insert_many([sensordata.model_dump() for sensordata in sensor_logs])
+    created_sensors = [tempsensor_collection.find_one({"_id": db_id}) for db_id in inserted_logs.inserted_ids]
+    return created_sensors
