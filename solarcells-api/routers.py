540f564741fd721ec128a06923e03dfc59d17f7c
Tobias Eriksson <tobias.eriksson@ha.ax>
2023-12-06 22:23:11 +0200

    Added scheduler logging functions

--- solarcells-api/routers.py
+++ solarcells-api/routers.py
@@ -1,13 +1,15 @@
-from fmiweather import get_latest_weather
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import HTTPException, APIRouter, Depends
 from datetime import datetime
-from models import PanelLog, Panel, CreateLogModel, Metadata
-from database import get_logs_collection, get_panels_collection
+from models import PanelLog, Panel, CreateLogModel
+from database import get_logs_collection, get_panels_collection, save_log_to_database
 from http import HTTPStatus
 from typing import List
 import datetime as dt
 
 router = APIRouter()
+scheduler = AsyncIOScheduler()
+scheduler_started = False
 
 
 @router.get("/logs", response_model=List[PanelLog], status_code=HTTPStatus.OK)
@@ -23,20 +25,10 @@ async def create_panel_log(
         logs_collection=Depends(get_logs_collection),
         panel_collection=Depends(get_panels_collection)
 ):
-    panel = await panel_collection.find_one({"panelID": panel_id})
-    if not panel:
+    panel = await save_log_to_database(log, panel_id, logs_collection, panel_collection)
+    if panel is None:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=f"Panel with ID {panel_id} not found")
-    panel_log = PanelLog(
-        timestamp=datetime.now().replace(microsecond=0),
-        metadata=Metadata(
-            effect=log.effect,
-            weather=get_latest_weather(),
-            panel=panel
-        )
-    )
-    new_log = await logs_collection.insert_one(panel_log.model_dump())
-    created_log = await logs_collection.find_one({"_id": new_log.inserted_id})
-    return created_log
+    return panel
 
 
 @router.get("/{panel_id}/logs", response_model=List[PanelLog], status_code=HTTPStatus.OK)
@@ -103,3 +95,35 @@ async def create_panel(panel: Panel, panels_collection=Depends(get_panels_collec
 async def get_all_panels(panels_collection=Depends(get_panels_collection)):
     panellist = await panels_collection.find().to_list(1000)
     return panellist
+
+
+@router.post('/start-logging')
+async def start_scheduler(
+        logs_collection=Depends(get_logs_collection),
+        panel_collection=Depends(get_panels_collection)
+):
+    log = CreateLogModel(
+        effect="123W"
+    )
+    panel_id = 1
+    global scheduler_started
+    if not scheduler_started:
+        async def job():
+            await save_log_to_database(log, panel_id, logs_collection, panel_collection)
+        scheduler.add_job(job, 'interval', minutes=2)
+        scheduler.start()
+        scheduler_started = True
+        return {"message": "Logger started"}
+    else:
+        raise HTTPException(status_code=400, detail="Logger is already running")
+
+
+@router.post('/stop-logging')
+async def stop_scheduler():
+    global scheduler_started
+    if not scheduler_started:
+        raise HTTPException(status_code=400, detail="Logger is not running")
+    scheduler.shutdown()
+    scheduler.remove_all_jobs()
+    scheduler_started = False
+    return {"message": "Logger shutdown"}
