6ce13de6e11359082d602d68c6d6e82743fec7d8
Tobias Eriksson <tobias.eriksson@ha.ax>
2023-12-16 00:49:34 +0200

    Added function to get a sensor

--- solarcells-api/database.py
+++ solarcells-api/database.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+from http import HTTPStatus
 
+from fastapi import HTTPException
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 from config.config import MONGO_DB, MONGO_URL
 from fmiweather import get_latest_weather
-from models import CreateLogModel, PanelLog, Metadata, TempSensorLog
+from models import CreateLogModel, PanelLog, Metadata, TempSensorLog, TemperatureSensor
 
 
 class MongoDB:
@@ -32,12 +34,16 @@ async def get_logs_collection():
     return await mongodb.get_collection("test-collection")
 
 
-async def get_tempsensor_collection():
+async def get_tempsensordata_collection():
     return await mongodb.get_collection("temp-sensor-data")
 
 
 async def get_panels_collection():
     return await mongodb.get_collection("panels")
+
+
+async def get_tempsensors_collection():
+    return await mongodb.get_collection("tempsensors")
 
 
 async def connect_to_db():
@@ -72,7 +78,7 @@ async def save_log_to_database(
 
 async def save_temp_log_to_database(
         vrm_sensor_data,
-        tempsensor_collection: AsyncIOMotorCollection
+        tempsensorlogs_collection: AsyncIOMotorCollection
 ):
     sensor_logs = await vrm_sensor_data()
     time_now = datetime.now().replace(microsecond=0)
@@ -80,15 +86,34 @@ async def save_temp_log_to_database(
     sensor_logs = [
         TempSensorLog(
             timestamp=time_now,
+            sensor=TemperatureSensor(
+              tempsensor_ID=29,
+              sensor_location="inside"
+            ),
             sensordata=sensor_logs[0],
             weather=weather
         ),
         TempSensorLog(
             timestamp=time_now,
+            sensor=TemperatureSensor(
+                tempsensor_ID=30,
+                sensor_location="outside"
+            ),
             sensordata=sensor_logs[1],
             weather=weather
         )
     ]
-    inserted_logs = await tempsensor_collection.insert_many([sensordata.model_dump() for sensordata in sensor_logs])
-    created_sensors = [tempsensor_collection.find_one({"_id": db_id}) for db_id in inserted_logs.inserted_ids]
-    return created_sensors
+    inserted_logs = await tempsensorlogs_collection.insert_many([sensordata.model_dump() for sensordata in sensor_logs])
+    created_logs = [tempsensorlogs_collection.find_one({"_id": db_id}) for db_id in inserted_logs.inserted_ids]
+    return created_logs
+
+
+async def get_tempsensor(
+        sensor_id,
+        tempsens_collection: AsyncIOMotorCollection
+):
+    sensor = tempsens_collection.find_one({"tempsensor_ID": sensor_id})
+    if not sensor:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=f"Sensor with ID {sensor_id} not found")
+
+    return TemperatureSensor(**sensor)
